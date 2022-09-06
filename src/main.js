21e51eec39d7a44919d96503fb3c0c0dce2fa7e9
何方 <he529564582@163.com>
2022-09-06 20:00:29 +0800

    引入axios和elementUI

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue"
+import App from "./App.vue"
+import { Button } from "element-ui"
 
 Vue.config.productionTip = false
+Vue.component(Button.name, Button)
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: h => h(App)
+}).$mount("#app")
